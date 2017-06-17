0393a10023ccaeb1b494b45b35c66d33ba6e5ab5
aliciasavelly <alicia.savelly@gmail.com>
2017-06-16 22:32:15 -0700

    remove files and reformat modal

--- db/seeds.rb
+++ db/seeds.rb
@@ -13,14 +13,3 @@ demo_user = User.create!(
   uid: demo_user_identity.id,
   name: "Guest User"
 )
-
-for i in 0...10 do
-  Blurb.create!(
-    body: File.read("app/assets/speech/speech#{i}.txt"),
-    user_id: demo_user.id,
-    title: "Blurb #{i + 1}"
-  )
-  if i == 5
-    sleep(60) # per-minute quota
-  end
-end
